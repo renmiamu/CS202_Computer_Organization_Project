dcf212a604f086a50216b760c8f793926177579e
renmiamu <12311016@mail.sustech.edu.cn>
2025-04-29 19:40:00 +0800

    ifetch

--- IFetch.v
+++ IFetch.v
@@ -8,10 +8,24 @@ module IFetch (
     input jalr,
     input [31:0] Alu_result,
     output wire [31:0] instruction
-    
+
 );
 reg [31:0] pc;
+always @(negedge clk ) begin
+    if (!rst) begin
+        pc<={32{1'b0}};
+    end
+    else begin
+        if (branch_result|jal) begin
+            pc<=pc+imm32;
+        end
+        else if (jalr) begin
+            pc<=Alu_result;
+        end else begin
+            pc<=pc+4;
+        end
+    end
+end
+prgrom urom( .clka(clk), .addra(pc[15:2]), .douta(inst));
 
-
-    
 endmodule
