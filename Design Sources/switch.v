96f80cad1d3da67ed1b3a5b0dd354e3d67d50a37
Hongli SHEN <149307453+renmiamu@users.noreply.github.com>
2025-05-15 14:00:41 +0800

    switch

--- Design Sources/switch.v
+++ Design Sources/switch.v
@@ -7,18 +7,18 @@ module switch (
     input confirmation,
     output reg [15:0] dataIOInput
 );
-always @(negedge clk) begin
-    if (~rst) begin
-        dataIOInput <= 16'b0;
+always @(posedge clk) begin
+    if (!rst) begin
+        dataIOInput <= {16{1'b0}};
     end else if (switchCtrl) begin
         case (address)
-            32'hffff_ff00: dataIOInput <= {15'b0, confirmation};                         
+            32'hffff_ff00: dataIOInput <= {{15{1'b0}}, confirmation};                         
             32'hffff_fff1: dataIOInput <= switchInput;                                   
             32'hffff_fff3: dataIOInput <= {{8{switchInput[15]}}, switchInput[15:8]};   
-            32'hffff_fff5: dataIOInput <= {8'b0, switchInput[15:8]};                     
-            32'hffff_fff7: dataIOInput <= {8'b0, switchInput[2:0]};                     
-            32'hffff_fff9: dataIOInput <= {8'b0, switchInput[7:0]};                     
-            default: dataIOInput <= 16'b0;
+            32'hffff_fff5: dataIOInput <= {{8{1'b0}}, switchInput[15:8]};                     
+            32'hffff_fff7: dataIOInput <= {{8{1'b0}}, switchInput[2:0]};                     
+            32'hffff_fff9: dataIOInput <= {{8{1'b0}}, switchInput[7:0]};                     
+            default: dataIOInput <= {16{1'b0}};
         endcase
     end
 end
