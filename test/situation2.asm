f075ea6b1a9d38a673d0a13dae876d3a86b9087d
Hongli SHEN <149307453+renmiamu@users.noreply.github.com>
2025-05-25 00:35:07 +0800

    sit2 case0-6

--- test/situation2.asm
+++ test/situation2.asm
@@ -57,25 +57,34 @@ not_palindrome:
     sw zero, 0(s10)  #??修改为led地址
     jal init
 
-
 case2:
     # 第一个浮点数输入
     jal switchjudge          # 等待确认
     li t2, 0xfffffff9        # SWITCH_DATA_ADDR
     lw t3, 0(t2)             # t2 = 输入的高8??
     sw t3, 0(s3)        # 存储浮点数到 memory[a]
-    jal decode_float12
-    srli a0, a0, 4
-    sw a0, 0(s9)        # 同步显示到数码管a
 
+    jal decode_float12
+ 
+    srli a0, a0, 4
+    beqz t0, print_pos_a 
+    sw a0, 0(s8)
+	
+           # 同步显示到数码管a
+input_b:
     # 第二个浮点数输入
     jal switchjudge
     li t2, 0xfffffff9
     lw t3, 0(t2)
     sw t3, 4(s3)        # 存储浮点数到 memory[b]
+    
     jal decode_float12
+  
     srli a0, a0, 4
-    sw a0, 0(s9)        # 同步显示?? LED 或数码管b
+    beqz t0, print_pos_b
+    sw a0, 0(s8)
+    #beq t0,s5, print_neg_b
+                 # 同步显示?? LED 或数码管b
     jal init
 decode_float12:
     andi t3, t3, 0xFF         # ????????8λ
@@ -92,18 +101,32 @@ decode_float12:
     bgez t1, shift_left
     neg t6, t1
     srl a0, a0, t6            # ???? E ?Σ????????
-    j finalize
+    j done
 
 shift_left:
     sll a0, a0, t1            # ???? E ?Σ????????
 
-finalize:            # ?????? 16
-    beqz t0, done
-    srli a0, a0, 4
-    sw a0, 0(s8)            # ??? S = 1?????
-    jal init 
 done:
     jr ra
+
+print_neg_a:
+    sw a0, 0(s8)
+    neg a0, a0 
+    j input_b
+    
+print_pos_a:
+    sw a0, 0(s9)
+    j input_b
+
+print_neg_b:
+    sw a0, 0(s8)
+    neg a0, a0 
+    j init
+    
+print_pos_b:
+    sw a0, 0(s9)
+    j init
+
 
 case3:
     #jal switchjudge
